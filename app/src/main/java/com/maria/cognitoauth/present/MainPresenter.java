c590d09af0a43b217b327f544e035c950ae29fb4
Maria <vasilevamariav@yandex.ru>
2018-07-21 01:58:40 +0400

    createUserPool

--- app/src/main/java/com/maria/cognitoauth/present/MainPresenter.java
+++ app/src/main/java/com/maria/cognitoauth/present/MainPresenter.java
@@ -1,12 +1,19 @@
 package com.maria.cognitoauth.present;
 
-import com.maria.cognitoauth.iview.MainView;
+import android.content.Context;
 
-public class MainPresenter {
+import com.maria.cognitoauth.iview.MainView;
+import com.maria.cognitoauth.model.network.AuthenticationProvider;
+
+public class MainPresenter implements AuthenticationProvider.Listener{
     private MainView view;
 
     public MainPresenter(MainView view) {
         this.view = view;
+    }
+
+    public void onCreate() {
+        AuthenticationProvider authProvider = new AuthenticationProvider(this, (Context) view);
     }
 
     public void detachView() {
